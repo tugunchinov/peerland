1f58bfd58cdef26ad653a60594998dfcaeaeb71f
Maxim Tugunchinov <tugunchinov.maxim@proton.me>
2025-02-20 21:03:26 +0300

    kv skeleton

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -1,1 +1,19 @@
-struct Storage {}
+struct KVStorage {}
+
+impl KVStorage {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn put(&self, key: String, value: String) -> bool {
+        todo!()
+    }
+
+    fn delete(&self, key: String) -> bool {
+        todo!()
+    }
+
+    fn get(&self, key: String) -> Option<String> {
+        todo!()
+    }
+}
